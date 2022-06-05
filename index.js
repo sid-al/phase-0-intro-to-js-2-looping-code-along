6a28cd86326b4b7df73f99837643210eea315e8a
sid-al <sendplusx@gmail.com>
2022-06-05 16:10:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(arry, event){
+    const nwArray=[];
+    for (let i =0; i< arry.length(); i++){
+        nwArray.push(`Thanks you, ${arry[i]}, for the wounderful ${event} gift!`)
+
+        return nwArray;
+    }
+
+    }
+
+    
+
